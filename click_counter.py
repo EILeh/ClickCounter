386731bac8116c8eb97133380aed7eb5d41ffa23
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-05-10 13:44:58 +0300

    Click counter

---
+++ click_counter.py
@@ -0,0 +1,39 @@
+"""
+COMP.CS.100 Ohjelmointi 1 / Programming 1
+Student Id: 0123456
+Name:       Xxxx Yyyyyy
+Email:      xxxx.yyyyyy@tuni.fi
+
+Code template for counter program.
+"""
+
+from tkinter import *
+
+
+class Counter:
+    def __init__(self):
+        # TODO: You have to creater one label and four buttons and store
+        #       them in the following attributes:
+        #
+        #       self.__current_value_label  # Label displaying the current value of the counter.
+        #       self.__reset_button         # Button which resets counter to zero.
+        #       self.__increase_button      # Button which increases the value of the counter by one.
+        #       self.__decrease_button      # Button which decreases the value of the counter by one.
+        #       self.__quit_button          # Button which quits the program.
+        #
+        #       Make sure you name the components exactly as shown above,
+        #       otherwise the automated tests will fail.
+
+    # TODO: Implement the rest of the needed methods here.
+
+
+def main():
+    # There is no need to modify the main function.
+    # As a matter of fact, automated tests ignore main
+    # once again.
+
+    Counter()
+
+
+if __name__ == "__main__":
+    main()
